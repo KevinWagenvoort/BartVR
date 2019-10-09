350c78d0dbb38f961fa0a757fa4889bbade64b42
Erwin van den Bor <evandenbor@msn.com>
2019-10-09 10:49:56 +0200

    New phone map

---
+++ Assets/MinimapSync.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapSync : MonoBehaviour
+{
+    public GameObject Arrow;
+    public GameObject CameraRig;
+    private Transform Eye;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Eye = CameraRig.transform.Find("Camera (eye)");
+        if (Eye == null)
+        {
+            Eye = CameraRig.transform.Find("Camera (head)").transform.Find("Camera (eye)");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Arrow.GetComponent<RectTransform>().transform.localRotation = Quaternion.Euler(90, 0, Eye.localEulerAngles.y -45);
+        Debug.Log(Eye.eulerAngles.y);
+    }
+}
