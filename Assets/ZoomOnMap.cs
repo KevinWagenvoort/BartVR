8e1b930b9ec8afc8d634efe0acc8d203da451a12
Erwin van den Bor <evandenbor@msn.com>
2019-10-10 10:15:58 +0200

    Zoom on control room map

---
+++ Assets/ZoomOnMap.cs
@@ -0,0 +1,51 @@
+﻿// https://forum.unity.com/threads/zoom-in-out-on-scrollrect-content-image.284655/
+// Thanks SomeVVhIteGuy!
+
+using UnityEngine;
+
+public class ZoomOnMap : MonoBehaviour
+{
+    [SerializeField] float startSize = 1;
+    [SerializeField] float minSize = 0.75f;
+    [SerializeField] float maxSize = 1;
+
+    [SerializeField] private float zoomRate = 5;
+
+    Vector3 difference;
+    Vector3 mousePos;
+
+    private void Update()
+    {
+        float scrollWheel = -Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollWheel != 0)
+        {
+            ChangeZoom(scrollWheel);
+        }
+    }
+
+    private void ChangeZoom(float scrollWheel)
+    {
+
+        float rate = 1 + zoomRate * Time.unscaledDeltaTime;
+        if (scrollWheel > 0 && transform.localScale.y > minSize)
+        {
+            mousePos = Input.mousePosition;
+            SetZoom(Mathf.Clamp(transform.localScale.y / rate, minSize, maxSize));
+            difference = transform.position - mousePos;
+            transform.position = mousePos + (difference * 0.9F);
+        }
+        else if (scrollWheel < 0 && transform.localScale.y < maxSize)
+        {
+            mousePos = Input.mousePosition;
+            SetZoom(Mathf.Clamp(transform.localScale.y * rate, minSize, maxSize));
+            difference = transform.position - mousePos;
+            transform.position = mousePos + (difference * 1.11F);
+        }
+    }
+
+    private void SetZoom(float targetSize)
+    {
+        transform.localScale = new Vector3(targetSize, targetSize, 1);
+    }
+}
