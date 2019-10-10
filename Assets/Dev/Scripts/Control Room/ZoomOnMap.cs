bb5ab153e7fef3a33ace3413e85a6954d6815748
Erwin van den Bor <evandenbor@msn.com>
2019-10-10 13:55:42 +0200

    Fixed map zoom

--- Assets/Dev/Scripts/Control Room/ZoomOnMap.cs
+++ Assets/Dev/Scripts/Control Room/ZoomOnMap.cs
@@ -1,11 +1,13 @@
 ﻿// https://forum.unity.com/threads/zoom-in-out-on-scrollrect-content-image.284655/
 // Thanks SomeVVhIteGuy!
 
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ZoomOnMap : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public Camera canvasCamera;
     [SerializeField] float startSize = 1;
     [SerializeField] float minSize = 0.75f;
     [SerializeField] float maxSize = 1;
@@ -37,21 +39,35 @@ public class ZoomOnMap : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
 
     private void ChangeZoom(float scrollWheel)
     {
-
+        PointerEventData cursor = new PointerEventData(EventSystem.current);
+        cursor.position = Input.mousePosition;
+        List<RaycastResult> objectsHit = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(cursor, objectsHit);
+        Vector2 localPoint;
+        foreach (RaycastResult result in objectsHit)
+        {
+            if (result.gameObject.name == "2DMap")
+            {
+                RectTransform rt = result.gameObject.GetComponent<RectTransform>();
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(rt, Input.mousePosition, canvasCamera, out localPoint);
+                mousePos = localPoint;
+                mousePos.z = 0;
+            }
+        }
         float rate = 1 + zoomRate * Time.unscaledDeltaTime;
         if (scrollWheel > 0 && transform.localScale.y > minSize)
         {
-            mousePos = Input.mousePosition;
             SetZoom(Mathf.Clamp(transform.localScale.y / rate, minSize, maxSize));
-            difference = transform.position - mousePos;
-            transform.position = mousePos + (difference * 0.9F);
+            difference = transform.localPosition - mousePos;
+            difference.z = 0;
+            transform.localPosition = mousePos + (difference * 0.9F);
         }
         else if (scrollWheel < 0 && transform.localScale.y < maxSize)
         {
-            mousePos = Input.mousePosition;
             SetZoom(Mathf.Clamp(transform.localScale.y * rate, minSize, maxSize));
-            difference = transform.position - mousePos;
-            transform.position = mousePos + (difference * 1.11F);
+            difference = transform.localPosition - mousePos;
+            difference.z = 0;
+            transform.localPosition = mousePos + (difference * 1.11F);
         }
     }
 
