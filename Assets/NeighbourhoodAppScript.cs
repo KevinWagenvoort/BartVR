e342c6d811d6e61d5bb2755f459d5058ea62ea6c
Erwin van den Bor <evandenbor@msn.com>
2019-11-01 11:45:31 +0100

    Seperate private and neighborhood chats

--- Assets/NeighbourhoodAppScript.cs
+++ Assets/NeighbourhoodAppScript.cs
@@ -8,11 +8,16 @@ public class NeighbourhoodAppScript : MonoBehaviour
 
     private Sender Vriend;
     private Sender Jij;
+    private List<string> possibleAnswers = new List<string>();
+    private int chosenAnswer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         ChatApp = new ChatApp();
+        possibleAnswers.Add("Is goedburger. Ik haal wel pizza. Zie je dan.");
+        possibleAnswers.Add("Is goed. Ik haal wel patat. Zie je dan.");
+        possibleAnswers.Add("Is goed. Ik haal wel chinees. Zie je dan.");
         Vriend = new Sender("Vriend", Sender.Role.Npc);
         Jij = new Sender("Jij", Sender.Role.Burger);
     }
@@ -23,11 +28,7 @@ public class NeighbourhoodAppScript : MonoBehaviour
         switch (passCount)
         {
             case 0:
-                List<string> possibleAnswers = new List<string>();
-                possibleAnswers.Add("Is goed. Ik haal wel pizza. Zie je dan.");
-                possibleAnswers.Add("Is goed. Ik haal wel patat. Zie je dan.");
-                possibleAnswers.Add("Is goed. Ik haal wel chinees. Zie je dan.");
-                ChatApp.Send("Yo zin om vanavond samen te eten?", Vriend, Message.Type.Question, possibleAnswers);
+                ChatApp.Send("Yo burger om vanavond samen te eten?", Vriend, Message.Type.Question, possibleAnswers);
                 break;
             case 1:
                 ChatApp.Send("Vanaf 7 uur ben ik thuis. Dan kan je wel komen.", Vriend, Message.Type.Other);
@@ -42,8 +43,10 @@ public class NeighbourhoodAppScript : MonoBehaviour
         passCount++;
     }
 
-    public void SendChoice(string message)
+    public void SendChoice(int messageNumber)
     {
-        ChatApp.Send(message, Jij, Message.Type.Answer);
+        ChatApp.Send(possibleAnswers[messageNumber], Jij, Message.Type.Answer);
+        chosenAnswer = messageNumber;
+        Invoke("Tutorial", 2);
     }
 }
