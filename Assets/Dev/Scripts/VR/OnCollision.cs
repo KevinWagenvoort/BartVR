d3a4754c0eed852d37c63c583b4a127e1856dc30
Erwin van den Bor <evandenbor@msn.com>
2019-11-08 14:48:33 +0100

    Test scenario trigger

--- Assets/Dev/Scripts/VR/OnCollision.cs
+++ Assets/Dev/Scripts/VR/OnCollision.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class OnCollision : MonoBehaviour {
 	void OnCollisionEnter(Collision collision) {
-		if(collision.gameObject.tag != "Hands" && collision.gameObject.tag != "POI" && collision.gameObject.tag != "Suspect") {
+		if(collision.gameObject.tag != "Hands" && collision.gameObject.tag != "POI" && collision.gameObject.tag != "Suspect" && collision.gameObject.tag != "ScenarioTrigger") {
 			SteamVR_Fade.Start(Color.black, 0f);
 		}
+
+        if(collision.gameObject.tag == "ScenarioTrigger")
+        {
+            Debug.Log("Scenario triggered");
+        }
 	}
 
 	void OnCollisionExit(Collision collision) {
