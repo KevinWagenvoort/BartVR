b8b906a2faafe980ce5e1d26829a975ba3defd83
Erwin van den Bor <evandenbor@msn.com>
2019-11-08 15:22:28 +0100

    Distance based scenario trigger

--- Assets/Dev/Scripts/VR/OnCollision.cs
+++ Assets/Dev/Scripts/VR/OnCollision.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class OnCollision : MonoBehaviour {
 	void OnCollisionEnter(Collision collision) {
-		if(collision.gameObject.tag != "Hands" && collision.gameObject.tag != "POI" && collision.gameObject.tag != "Suspect" && collision.gameObject.tag != "ScenarioTrigger") {
+		if(collision.gameObject.tag != "Hands" && collision.gameObject.tag != "POI" && collision.gameObject.tag != "Suspect") {
 			SteamVR_Fade.Start(Color.black, 0f);
 		}
-
-        if(collision.gameObject.tag == "ScenarioTrigger")
-        {
-            Debug.Log("Scenario triggered");
-        }
 	}
 
 	void OnCollisionExit(Collision collision) {
---
+++ Assets/DistanceTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceTrigger : MonoBehaviour
+{
+    public GameObject CameraRig, Phone, LeftHand;
+    private bool phoneIsActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(CameraRig.transform.position, gameObject.transform.position);
+        if (distance < 53 && phoneIsActive)
+        {
+            Phone.SetActive(false);
+            LeftHand.SetActive(true);
+            phoneIsActive = false;
+            Debug.Log("enter");
+        } else if (distance >= 53 && !phoneIsActive)
+        {
+            Phone.SetActive(true);
+            LeftHand.SetActive(false);
+            phoneIsActive = true;
+            Debug.Log("exit");
+        }
+    }
+}
