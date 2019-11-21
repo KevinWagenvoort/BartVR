afbc207f386d5e9737b758c649c1f819dc3a1b86
Kevin <kevinwagenvoort@gmail.com>
2019-11-21 13:05:24 +0100

    No more trillen if not sending messageges

--- Assets/Dev/Scripts/Chat/MobileNeigborhoodChat.cs
+++ Assets/Dev/Scripts/Chat/MobileNeigborhoodChat.cs
@@ -77,7 +77,10 @@ public class MobileNeigborhoodChat : MonoBehaviour
             }
             else
             {
-                StartCoroutine(Vibrations.LongVibration(0.5f, 250));
+                if (DistanceTrigger.StartedSendingMessages)
+                {
+                    StartCoroutine(Vibrations.LongVibration(0.5f, 250));
+                }
                 if (LastMessage.type == Message.Type.Location)
                 {
                     newMessage = Instantiate(ReceivedLocationBubble, ReceivedLocationBubble.transform.parent);
