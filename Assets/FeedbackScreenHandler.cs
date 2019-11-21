28469b5bdb5e79bae6b76b83bc1b19f48b30697c
Kevin <kevinwagenvoort@gmail.com>
2019-11-21 10:43:53 +0100

    FeedbackScreen v1

---
+++ Assets/FeedbackScreenHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FeedbackScreenHandler : MonoBehaviour
+{
+    public Button BackToMenu;
+    
+    void Start()
+    {
+        BackToMenu.onClick.AddListener(GoBackToMenu);
+    }
+
+    void GoBackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
