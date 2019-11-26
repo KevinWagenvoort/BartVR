5cef72e9b401e5b527af1e62ae864da7413201e5
Kevin <kevinwagenvoort@gmail.com>
2019-11-26 15:32:25 +0100

    Added colliders

---
+++ Assets/VRButtonHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRButtonHandler : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("VRButtonHandler");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter " + other.gameObject.name);
+    }
+}
