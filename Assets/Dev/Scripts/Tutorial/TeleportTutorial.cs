8c7187be3f49f174716fd7a2f66fbb82a38b1ec9
Erwin van den Bor <evandenbor@msn.com>
2019-12-04 12:55:34 +0100

    Fixed teleport tutorial

--- Assets/Dev/Scripts/Tutorial/TeleportTutorial.cs
+++ Assets/Dev/Scripts/Tutorial/TeleportTutorial.cs
@@ -2,19 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class TeleportTutorial : MonoBehaviour
 {
-
+    public SteamVR_Action_Boolean teleportAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Teleport");
     private int teleportCount = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void Update()
     {
+        if (teleportAction.GetStateUp(SteamVR_Input_Sources.RightHand) || teleportAction.GetStateUp(SteamVR_Input_Sources.LeftHand))
+        {
+            teleportCount++;
+        }
+        if (teleportCount > 2)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
