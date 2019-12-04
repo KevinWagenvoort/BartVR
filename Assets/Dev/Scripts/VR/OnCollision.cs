74debbd5877c9e65f228bf6bb3dbac62fc7613cc
Erwin van den Bor <evandenbor@msn.com>
2019-12-04 11:42:19 +0100

    Fixed collision fade

--- Assets/Dev/Scripts/VR/OnCollision.cs
+++ Assets/Dev/Scripts/VR/OnCollision.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class OnCollision : MonoBehaviour {
 	void OnCollisionEnter(Collision collision) {
 		if(collision.gameObject.tag != "Hands" && collision.gameObject.tag != "POI" && collision.gameObject.tag != "Suspect") {
-			//SteamVR_Fade.Start(Color.black, 0f);
+            Debug.Log(collision.collider.name);
+			SteamVR_Fade.Start(Color.black, 0f);
 		}
 	}
 
 	void OnCollisionExit(Collision collision) {
-		//SteamVR_Fade.Start(Color.clear,0f);
+		SteamVR_Fade.Start(Color.clear,0f);
 	}
 }
