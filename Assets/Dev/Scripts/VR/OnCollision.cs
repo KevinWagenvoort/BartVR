cc938925b711065280bc9494dca3bbae0e47514b
Erwin van den Bor <evandenbor@msn.com>
2019-12-06 14:51:20 +0100

    Collision fixes

--- Assets/Dev/Scripts/VR/OnCollision.cs
+++ Assets/Dev/Scripts/VR/OnCollision.cs
@@ -7,7 +7,7 @@ public class OnCollision : MonoBehaviour {
     public GameObject Teleporting;
 
 	void OnTriggerEnter(Collider collision) {
-		if(collision.gameObject.tag != "Hands" && collision.gameObject.tag != "POI" && collision.gameObject.tag != "Suspect") {
+		if(collision.gameObject.tag != "Hands" && collision.gameObject.tag != "POI" && collision.gameObject.tag != "Suspect" && collision.gameObject.tag != "Officer" && collision.gameObject.tag != "VRUIButton") {
 			SteamVR_Fade.Start(Color.black, 0f);
             Teleporting.SetActive(false);
 		}
