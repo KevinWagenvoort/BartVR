574113d09a15e89f99d00ec3300dfde076b8f53d
Erwin van den Bor <evandenbor@msn.com>
2019-12-17 14:54:26 +0100

    Show Vive tutorial when not using Oculus hmd

--- Assets/Dev/Scripts/Tutorial/TeleportTutorial.cs
+++ Assets/Dev/Scripts/Tutorial/TeleportTutorial.cs
@@ -2,12 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 using Valve.VR;
 
 public class TeleportTutorial : MonoBehaviour
 {
     public SteamVR_Action_Boolean teleportAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Teleport");
+    public Sprite ViveSprite;
     private int teleportCount = 0;
+
+    private void Start()
+    {
+        if (!XRDevice.model.Contains("Oculus"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = ViveSprite;
+        }
+    }
 
     // Update is called once per frame
     void Update()
